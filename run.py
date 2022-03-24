e872a4d89c47c29795b3a144845c46574474c221
Ericolssoon <eric_olsson@hotmail.se>
2022-03-24 16:48:31 +0000

    Add board structure and function comments

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = [["-", "-", "-"],
+          ["-", "-", "-"],
+          ["-", "-", "-"]]
+
+# print out the board
+def print_board():
+    pass
+
+# check for win
+
+# check for tie
+
+# start game
