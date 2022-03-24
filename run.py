a1e82d53e8b75d3d82632db5e207bd819c6b11ae
Ericolssoon <eric_olsson@hotmail.se>
2022-03-24 17:59:21 +0000

    Add functions that checks for win, change from player X to 0

--- run.py
+++ run.py
@@ -2,12 +2,55 @@ board = [["-", "-", "-"],
           ["-", "-", "-"],
           ["-", "-", "-"]]
 
-# print out the board
+# print the board
+
+
 def print_board():
-    pass
+    print(board[0][0] + "  " + board[0][1] + "  " + board[0][2])
+    print(board[1][0] + "  " + board[1][1] + "  " + board[1][2])
+    print(board[2][0] + "  " + board[2][1] + "  " + board[2][2])
 
 # check for win
 
+
+def has_winner():
+    return False
+
 # check for tie
 
+
+def is_tie():
+    return False
+
 # start game
+
+
+def play(player_turn):
+    print_board()
+  while True:
+    print("Player " + player_turn + ", your turn.")
+
+    row = input("Choose row: ")
+    column = input("Choose column: ")
+    row = int(row) - 1
+    column = int(column) - 1
+
+    board[row][column] = player_turn
+
+    print_board()
+
+    # check for win or tie
+    if has_winner():
+      print("Player " + player_turn + " Won!")
+      break
+    elif is_tie():
+      print("It it a tie!.")
+      break
+
+    # change player
+    if player_turn == "X":
+      player_turn = "0"
+    else:
+      player_turn = "X"
+
+play("X")
